02614068eb85607a4046dc0c827edbe65063eaa7
yunf13 <yunf13@gmail.com>
2019-01-28 13:00:27 -0800

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,29 @@
 from sqlalchemy import Column, String
 from database import Base
 
+class Product(Base):
+    __tablename__ = 'products'
+
+    brand = Column(String(50), primary_key=True)
+    name = Column(String(100))
+    ingredients = Column(String(1000))
+    listPrice = Column(String(10))
+    size = Column(String(10))
+    rating = Column(String(10))
+
+    def __init__(self, brand=None, name=None, ingredients=None,
+                 listPrice=None,size=None,rating=None):
+        self.brand = brand
+        self.name = name
+        self.ingredients= ingredients
+        self.listPrice = listPrice
+        self.size = size
+        self.rating = rating
+
+
+    def __repr__(self):
+        return '<Product %r>' % (self.name)
+
 class Ingredient(Base):
     __tablename__ = 'ingredients'
 
