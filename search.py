17c7004a4b6671e4ca65f771690cb8a7c070a934
yunf13 <yunf13@gmail.com>
2019-01-28 13:02:25 -0800

    Update search.py

--- search.py
+++ search.py
@@ -1,12 +1,43 @@
 from elasticsearch import Elasticsearch
 
 endpoint = ('https://search-insightprojecttest-qcbe6ffjwxsdscktijg6uragwi.us-west-1.es.amazonaws.com')
+product_index = 'product'
 ingredient_index = 'ingredient_2'
 
 def init_search(endpoint=endpoint):
     es = Elasticsearch(endpoint)
     return es
 
+def product_search(brand,name, es, index=product_index):
+    """Give single product brand/ name, return Product(brand,name,ingredients,listPrice,size,rating)
+    """
+    query = {
+      "from": 0,
+      "size": 1,
+      "query": {
+        "bool": {
+          "must": [
+            { "match": { "brand": brand}}, 
+            { "match": { "name": name}}  
+          ]
+              
+        }
+      }
+    }
+    try:
+        result = es.search(index=index,
+                           body=query
+                          )['hits']['hits'][0]['_source']
+        brand = result['brand']
+        name = result['name']
+        ingredients = result['ingredients']
+        listPrice = result['listPrice']
+        size = result['size']
+        rating = result['rating']
+        return(brand, name, ingredients, listPrice, size, rating)
+    except:
+        return None
+    
 def ingredient_search(ing_name, es, index=ingredient_index):
     """Give single ingredient name, return Ingredient(name, about, safty, function)
     """
