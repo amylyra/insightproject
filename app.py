de57ad655018f604da5b598e3fd880e28bbb5655
Amy Yuan <amy@lyrahealth.com>
2019-01-29 15:12:15 -0800

    commit draft code for app.py

--- app.py
+++ app.py
@@ -20,6 +20,56 @@ def shutdown_session(exception=None):
 
 @app.route('/')
 def index():
+    """
+    Amy Yuan [2:26 PM]
+    1. search by product brand and name
+    2. return product ingredient list from ES
+    3. clean up
+    ingredient_return_list = [] (edited)
+    4. for every single ingredient in the ingredient list:
+    ingredient_return_list += ES.search(single_ingredient, index) (edited)
+    ingredient_return_list
+    has all the search outcome of ingredient extracted from ES.ingredient
+    5. display all the extracted ingredients by name, function, safety rating, blablabla.
+    in a table
+    to frontend.
+    """
+
+    # Search by product brand and name
+    if request.method == "POST":
+        db.remove()
+        search_brand_name = request.form['brand_name']
+        search_product_name = request.form['product_name']
+        es = init_search()
+        product_result = product_search(search_brand_name, search_product_name, es) #TO_DO
+
+    # step 2 and 3
+        if product_result:
+            ing_raw_list = product_result['Ingredient']
+        ing_clean_list = process_ingredient(ing_raw_list) #TO_DO
+
+    # step 4
+        ing_canonical_list = []
+        for i in ing_clean_list:
+            ing_result = ingredient_search(i, es)
+                if ing_result:
+                    new_ingredient = Ingredient()
+                    db_session.add(new_ingredient)
+                    db_session.commit()
+                    data = {
+                        "name": new_ingredient.name,
+                        "about": new_ingredient.about,
+                        "safety": new_ingredient.safety,
+                        "function": new_ingredient.function,
+                        }
+
+        print(data)
+        pusher_client.trigger('table', 'new-record', {'data': data })
+
+
+
+        ing_can_list.append
+
     ingredients = Ingredient.query.all()
     return render_template('index.html', ingredients=ingredients)
 
